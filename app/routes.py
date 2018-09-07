08db509b8325aa166321ba2f9092fb0fbc3f3488
Victoria Nascimento Meneghel <victoria@localhost.localdomain>
2018-09-07 13:49:11 +0200

    'looking  to solve the issues'

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.forms import LoginForm
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm
 from app.models import User
 
 
