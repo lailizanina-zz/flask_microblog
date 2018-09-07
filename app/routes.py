5cc13ee1480941ad074ac78513b89436a2c84bcf
Victoria Nascimento Meneghel <victoria@localhost.localdomain>
2018-09-07 14:23:35 +0200

    end cha.5

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-#importing from the packdege app the variable app(in __init__).
 from app.forms import LoginForm
-from flask_login import login_user, logout_user, current_user, login_required
+from flask_login import login_required, login_user, logout_user, current_user
 from werkzeug.urls import url_parse
 from app import app, db
+from app.forms import RegistrationForm
 from app.forms import LoginForm
 from app.models import User
 
@@ -54,3 +54,17 @@ def login():
     def logout():
         logout_user()
         return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
