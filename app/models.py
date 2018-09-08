e7cd7ab964d96bc60a933aa471d62ef9d3c10595
Victoria Nascimento Meneghel <victoria.nascimento.meneghel@gmail.com>
2018-09-08 20:16:09 +0200

    avatars

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 from app import db, login
 from flask_login import UserMixin
@@ -20,6 +21,10 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 @login.user_loader
 def load_user(id):
