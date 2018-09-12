f250ab6f3969a994896acff5a6de5977d95b6f02
Victoria Nascimento Meneghel <victoria.nascimento.meneghel@gmail.com>
2018-09-12 20:36:56 +0200

    followers db

--- app/models.py
+++ app/models.py
@@ -13,6 +13,11 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+#This is a association table
+    followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
