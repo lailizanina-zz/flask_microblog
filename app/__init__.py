ae5246a5e8d313c05d54cca45fe488966b84570b
Victoria Nascimento Meneghel <victoria.nascimento.meneghel@gmail.com>
2018-09-23 17:27:34 +0200

    bootstrap template included

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 from config import Config
 
 #app variable to give the name of the module, which responds to the file name.
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 #telling to get email. add a SMTPHandler intance to the Flask logger object/app.logger
 if not app.debug:
