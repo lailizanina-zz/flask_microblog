45c30f3b054b43ee4bbdc274c7047e4214eb68b7
Victoria Nascimento Meneghel <victoria.nascimento.meneghel@gmail.com>
2018-10-03 15:38:19 +0200

    comad short-cut to translations

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from flask_babel import _, get_locale
@@ -14,6 +14,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+    g.locale = str(get_locale())
 
 #this function is a call back to the event in which the function is associated to the URL / and /index.
 @app.route('/', methods=['GET', 'POST'])
--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 ##importing from the packdege app the variable app(in __init__) and app function(route).
-from app import app, db
+from app import app, db, cli
 from app.models import User, Post
 
 @app.shell_context_processor
