4f6e5606a4af3aa753bf5f823c1685e465cd2c28
Victoria Nascimento Meneghel <victoria.nascimento.meneghel@gmail.com>
2018-12-06 08:17:36 +0100

    gunicorn add and LOG_TO_STDOUT config to Heroku insert

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ from config import Config
 #some are defining variables to represent the modules exported
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
@@ -30,7 +31,8 @@ if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            auth = (app.config['MAIL_USERNAME'],
+                    app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
@@ -42,17 +44,23 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/microblog.log',
+                                        maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
 
 #giving the user language preferences
 @babel.localeselector
--- config.py
+++ config.py
@@ -10,6 +10,7 @@ class Config(object):
 #disable the ALCHEMY configuration to sign the aplication every time a change is about to be made in the database.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 #two expressions(a value sourced from an environment variable and a hardcored string, used in case the variable isn't disponible) joined by the or operator
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
